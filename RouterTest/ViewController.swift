7ea89e956c32a85be2ed4213a66de74ba3dac89d
Alex <abastide@gmail.com>
2018-07-25 11:50:16 +0200

    Initial Commit

---
+++ RouterTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RouterTest
+//
+//  Created by Alexis BASTIDE (EXT) on 25/07/2018.
+//  Copyright © 2018 Alexis BASTIDE (EXT). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
